7ceb3797f59fb8810da50cbbf590f1788099d498
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-10-04 14:11:30 -0500

    Formulario de Login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,7 +9,7 @@ class LoginScreen extends StatelessWidget {
         child: SingleChildScrollView(
             child: Column(
           children: [
-            SizedBox(height: 180),
+            SizedBox(height: 200),
             CardContainer(
                 child: Column(
               children: [
@@ -19,11 +19,49 @@ class LoginScreen extends StatelessWidget {
                   style: Theme.of(context).textTheme.headline4,
                 ),
                 SizedBox(height: 30),
-                Text('Formulario')
+                _LoginForm()
               ],
             )),
+            SizedBox(height: 50),
+            Text(
+              'Crear una nueva cuenta',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            )
           ],
         )),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO: mantener la referencia al KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.deepPurple, width: 2)),
+                  hintText: 'yourname@example.com',
+                  labelText: 'Correo electrónico',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  prefixIcon: Icon(
+                    Icons.alternate_email_sharp,
+                    color: Colors.deepPurple,
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
