c4b0041135a2865ae956333c3aedc54f3e7d74e1
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-10-18 09:52:26 -0500

    paciente card parte 2

--- lib/widgets/paciente_card.dart
+++ lib/widgets/paciente_card.dart
@@ -12,7 +12,13 @@ class PacienteCard extends StatelessWidget {
         decoration: _cardBorders(),
         child: Stack(
           alignment: Alignment.bottomLeft,
-          children: [_BackgroundImage(), _ProductDetails()],
+          children: [
+            _BackgroundImage(),
+            _PacienteDetails(),
+            Positioned(top: 0, right: 0, child: _DateTag()),
+            // TODO: mostrar de manera condicional
+            Positioned(top: 0, left: 0, child: _NotAvailable())
+          ],
         ),
       ),
     );
@@ -27,15 +33,88 @@ class PacienteCard extends StatelessWidget {
           ]);
 }
 
-class _ProductDetails extends StatelessWidget {
+class _NotAvailable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'No disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
       height: 70,
-      color: Colors.indigo,
+      decoration: BoxDecoration(
+        color: Colors.yellow[800],
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25), bottomRight: Radius.circular(25)),
+      ),
     );
   }
+}
+
+class _DateTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            '18/10/2022',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 115,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(25), bottomLeft: Radius.circular(25))),
+    );
+  }
+}
+
+class _PacienteDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 50),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _buildBoxDecoration(),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            'Paciente: Emanuel Rangel ',
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Text(
+            'Id del Paciente',
+            style: TextStyle(fontSize: 15, color: Colors.white),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ]),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+      color: Colors.indigo,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
 }
 
 class _BackgroundImage extends StatelessWidget {
