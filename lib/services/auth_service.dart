d31d5204ec830e0af221d6da751fe81fb107f9c2
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-11-22 15:15:01 -0600

    AuthProvider peticion para crear cuentas

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService extends ChangeNotifier {
+  final String _baseUrl = 'identitytoolkit.googleapis.com';
+  final String _firebaseToken = 'AIzaSyCovV4oxad7O79knxCNzuEo42HvswLgapo';
+
+  Future<String?> createUser(String email, String password) async {
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password
+    };
+
+    final url =
+        Uri.https(_baseUrl, '/v1/accounts:signUp', {'key': _firebaseToken});
+
+    final resp = await http.post(url, body: json.encode(authData));
+    final Map<String, dynamic> decodeResp = json.decode(resp.body);
+
+    print(decodeResp);
+  }
+}
