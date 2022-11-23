5a0713b8540612e976def5fd7e56764ddc58206b
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-11-22 18:21:30 -0600

    grabar token en el secure storage

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _firebaseToken = 'AIzaSyCovV4oxad7O79knxCNzuEo42HvswLgapo';
+
+  final storage = FlutterSecureStorage();
 
 // Si retornamos algo es un error, si no, todo bien
   Future<String?> createUser(String email, String password) async {
@@ -22,6 +25,7 @@ class AuthService extends ChangeNotifier {
 
     if (decodeResp.containsKey('idToken')) {
       // Token hay que guardarlo en un lugar seguro
+      await storage.write(key: 'token', value: decodeResp['idToken']);
       // return decodeResp['idToken'];
       return null;
     } else {
@@ -44,9 +48,16 @@ class AuthService extends ChangeNotifier {
     if (decodeResp.containsKey('idToken')) {
       //   // Token hay que guardarlo en un lugar seguro
       // return decodeResp['idToken'];
+      await storage.write(key: 'token', value: decodeResp['idToken']);
       return null;
     } else {
       return decodeResp['error']['message'];
     }
+  }
+
+  Future logout() async {
+    await storage.delete(key: 'token');
+
+    return;
   }
 }
